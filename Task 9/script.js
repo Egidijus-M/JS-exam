890fb3143695ad78dfb00278091162d618d8601c
Egidijus-M <egidijus.macernis@codeacademylt.onmicrosoft.com>
2022-02-24 20:56:04 +0200

    9 task completed.

---
+++ Task 9/script.js
@@ -0,0 +1,24 @@
+/* ------------------------------ TASK 9 ---------------------------------------------------------------
+Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri sukuria objektus 3 savybėm ir 1 metodu:
+
+Savybės:
+title, director, budget
+Metodas: 
+wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
+------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+
+  wasExpensive() {
+    return this.budget > 100000000;
+  }
+}
+
+const gladiator = new Movie("Gladiator", "Ridley Scott", 103000000);
+
+console.log(`Was "Gladiator" expensive move? ${gladiator.wasExpensive()}`);
